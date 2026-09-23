e6477229a19e2d409821464d69a06780e86c6656
lichen1972 <lichen.ng@gmail.com>
2026-09-23 16:42:26 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guitar-songs-board-v3";
+const CACHE_NAME = "guitar-songs-board-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.png"];
 
 self.addEventListener("install", (event) => {
